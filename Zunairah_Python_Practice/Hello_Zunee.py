f77a822ca3f4efed5e8f889beae692be1ba17fe1
arsal3646 <arsal3646@gmail.com>
2025-12-04 21:40:49 +0400

    Renamed folders

--- Zunairah_Python_Practice/Hello_Zunee.py
+++
@@ -1,9 +0,0 @@
-print("Hello, I am Zunairah, I want to learn Python programming!")
-
-//  Tasks 1. Find Square root of 900 and 9 and 81 using Python
-
-
-
-
-
-// Task 2. Find 2 raise to power 6    .... find 3 raise to power 4.... find 5 raise to power 3 using Python
